2df2e9a9c4dc5e3cd87287cf761b296d1211c999
pengsicheng <pengsicheng@didichuxing.com>
2018-10-18 21:11:53 +0800

    Updates

---
+++ precache-manifest.89ead03743d85469ce538cb8042b929b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2befbb39b9444f42a972875a5c67601a",
+    "url": "/Random-Sth/static/media/bg4.2befbb39.jpg"
+  },
+  {
+    "revision": "509768069962044707b8b30e816bf245",
+    "url": "/Random-Sth/static/media/bg1.50976806.png"
+  },
+  {
+    "revision": "c4e0e65b42ab5ba647fd",
+    "url": "/Random-Sth/static/js/runtime~main.c4e0e65b.js"
+  },
+  {
+    "revision": "110af778037c2aa8d711",
+    "url": "/Random-Sth/static/js/main.110af778.chunk.js"
+  },
+  {
+    "revision": "a47b2787cc97e853bc4c",
+    "url": "/Random-Sth/static/js/1.a47b2787.chunk.js"
+  },
+  {
+    "revision": "110af778037c2aa8d711",
+    "url": "/Random-Sth/static/css/main.f5f657ef.chunk.css"
+  },
+  {
+    "revision": "a47b2787cc97e853bc4c",
+    "url": "/Random-Sth/static/css/1.c19821c1.chunk.css"
+  },
+  {
+    "revision": "b0c407acaf3615275601b1a43a4bcf1b",
+    "url": "/Random-Sth/index.html"
+  }
+];
